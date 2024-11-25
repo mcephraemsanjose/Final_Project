c9736a103ea9d2ae95da9f1dcaf145f70d40ce3e
mcephraemsanjose <sanjosemcephraem_bsit@plmun.edu.ph>
2024-11-25 23:46:51 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,79 @@
+ <!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <link rel="stylesheet" href="styles.css">
+
+    <link rel="stylesheet" href="header.css">
+
+    <link rel="stylesheet" href="profile.css">
+
+    <link rel="stylesheet" href="home.css">
+
+    <script src="profile.js"></script>
+
+    <title>Beez-shop</title>
+
+</head>
+
+<body style="background-image: url('image/bees_920355-35.avif'); background-size: cover; background-repeat: no-repeat; background-position: center; background-attachment: fixed; color: white;">
+
+    <ul class="nav">
+
+        <li class="logo"><img src="icon/Beezzz.webp" alt=""></li>
+
+        <li><a onclick="on()"><span class="material-symbols-outlined">face</span></a></li>
+
+        <li><a href="about.php"><span class="material-symbols-outlined">info</span></a></li>   
+
+        <li><a href="cart.php"><span class="material-symbols-outlined">shopping_cart</span></a></li>
+
+        <li><a href="menu.php"><span class="material-symbols-outlined">menu_book</span></a></li>
+
+        <li><a href="home.php"><span class="material-symbols-outlined">home</span></a></li>
+
+    </ul>
+
+    <section>
+
+        <div class="intro">
+
+            <img src="image/Beezzz-shop.png" alt="logo">
+
+            <div class="text">
+
+                <h1>Welcome to <br> Beez-shop</h1>
+
+                <p>Welcome to Beez-Shop, a unique, buzzing experience where honey isn't just an ingredient—it's the star of the show! Think of it like your cozy neighborhood coffee shop, but with a delightful twist: honey in every creation. Whether you're a connoisseur of sweet, a health-conscious customer, or simply in search of a new experience, Beez-Shop offers a menu crafted around the natural goodness of honey.</p>
+
+                <button><a href="menu.php">Order Now!</a></button>
+
+            </div>
+
+        </div>
+
+    </section>
+
+
+<footer>
+
+    <p>Created by Group 8.corp.</p>
+
+</footer>
+
+
+</body>
+
+</html>
