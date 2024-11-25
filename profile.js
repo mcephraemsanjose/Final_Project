c66904af8db0c55ece39d17c9d1536f797987a43
mcephraemsanjose <sanjosemcephraem_bsit@plmun.edu.ph>
2024-11-25 23:37:42 +0800

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,14 @@
+ function on() {
+
+    document.getElementById("profile").style.display = "block";
+
+  }
+
+
+function off(){
+
+  document.getElementById("profile").style.display = "none";
+
+}
+
+ 
